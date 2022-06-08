2d312ce6e48632b481a342e39d169ab08fc095aa
Ethan Smith <ethansmith.dev@gmail.com>
2022-06-08 00:08:14 -0400

    Fixed projection dot system

--- src/user_interface.rs
+++ src/user_interface.rs
@@ -5,7 +5,7 @@ use bevy::{
     render::view::VisibleEntities,
 };
 
-use super::physics::{Kinimatics, KinimaticsBundle};
+use super::physics::Kinimatics;
 use super::ships::{Engine, Throttle};
 
 pub struct UserInterfacePlugin;
@@ -13,9 +13,9 @@ pub struct UserInterfacePlugin;
 impl Plugin for UserInterfacePlugin {
     fn build(&self, app: &mut App) {
         app.add_startup_system(startup_system)
+            .add_system(user_interface_system)
             .add_system(course_projection_system)
-            .add_system(user_interface_system);
-            //.add_startup_system(init_ui);
+            ;
     }
 }
 
@@ -39,7 +39,6 @@ pub struct UISprites {
 
 fn startup_system(
     mut commands: Commands,
-    mut materials: ResMut<Assets<ColorMaterial>>,
     asset_server: ResMut<AssetServer>,
 ) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
@@ -79,30 +78,24 @@ fn user_interface_system(
 ) {
     // handle zooming when the user scrolls
     for event in wheel_evr.iter() {
-        for (mut ortho, _transform, mut camera, mut entities) in cam_query.iter_mut() {
+        for (mut ortho, _transform, mut camera, _entities) in cam_query.iter_mut() {
             const ZOOM_SPEED: f32 = 0.1;
             let scale_difference = (10.0 as f32).powf(event.y as f32 * ZOOM_SPEED);
 
             // adjust camera scaling
-            ortho.far += (1000.0 / (0.1 * event.y)).abs();
             ortho.scale *= scale_difference;
             camera.projection_matrix = ortho.get_projection_matrix();
 
             // scale visible entities
-            for &mut e in entities.entities.iter_mut() {
-                match transform_query.get_component_mut::<Transform>(e) {
-                    Ok(mut t) => {
-                        t.scale *= Vec3::ONE * scale_difference;
-                    }
-                    Err(_) => (),
-                };
+            for mut t in transform_query.iter_mut() {
+                t.scale *= Vec3::ONE * scale_difference;
             }
         }
     }
 
     // handle paning when the user middle clicks and drags
 
-    for (mut op, mut t, c, _e) in cam_query.iter_mut() {
+    for (op, mut t, _c, _e) in cam_query.iter_mut() {
         if mouse_state.pressed(MouseButton::Middle) {
             motion_evr
                 .iter()
@@ -123,7 +116,7 @@ pub fn course_projection_system(
     sprites: Res<UISprites>,
 ) {
     // make a copy of all the entities
-    let mut entities: Vec<(Kinimatics, Transform, Option<Engine>)> = k_bods
+    let entities: Vec<(Kinimatics, Transform, Option<Engine>)> = k_bods
         .iter()
         .map(|(kinimatics, transform, engine)| {
             if let Some(e) = engine {
@@ -134,77 +127,77 @@ pub fn course_projection_system(
         })
         .collect();
 
-    let num_seconds = 5; // number of seconds to look ahead
-    let step_precision = 10; // steps/second
+    let num_seconds = 25; // number of seconds to look ahead
+    let step_precision = 15; // steps/second
 
-    let mut steps: Vec<Vec<Transform>> = Vec::new();
+    let mut steps: Vec<Vec<(Kinimatics, Transform, Option<Engine>)>> = Vec::new();
     steps.reserve(num_seconds * step_precision);
 
-    // each element will have a corresponding entry in this list.
-    let num_bods = entities.iter().count();
-    let mut all_forces: Vec<Vec<Vec3>> = Vec::new();
-    all_forces.reserve(num_bods);
-    for _ in 0..num_bods {
-        all_forces.push(Vec::new());
+    let mut forces: Vec<Vec3> = Vec::new();
+    forces.reserve(entities.len());
+    for _ in 0..entities.len() {
+        forces.push(Vec3::ZERO);
     }
+
+    // initial state
+    steps.push(entities.clone());
 
     // account for force due to gravity
     const GRAVITATIONAL_CONSTANT: f32 = 6.67430e-11;
-    for step in 0..num_seconds * step_precision {
-        let dt = 1.0 / (step_precision as f32);
+    let dt = 1.0 / (step_precision as f32);
+    for step in 1..num_seconds * step_precision {
+        steps.push(steps[step-1].clone());
 
-        // calculate forces due to gravity
-        for (i, e1) in entities.iter().enumerate() {
-            // NOTE do I need to do bounds checking here?
-            entities
-                .split_at(i + 1)
+        // calculate forces for each body
+        for (i, bod1) in steps[step].iter().enumerate() {
+            let (k1, t1, engine) = bod1;
+
+            // add forces due to gravity
+            steps[step]
+                .split_at(i+1)
                 .1
                 .iter()
                 .enumerate()
-                .for_each(|(j, e2)| {
+                .for_each(|(j, bod2)| {
+                    let (k2, t2, _) = bod2;
+
                     // calculate magnitude of the force
-                    let force_mag = GRAVITATIONAL_CONSTANT * (e1.0.mass * e2.0.mass)
-                        / e1.1.translation.distance_squared(e2.1.translation);
+                    let force_mag = GRAVITATIONAL_CONSTANT * (k1.mass * k2.mass)
+                        / t1.translation.distance_squared(t2.translation);
 
                     // calculate direction and magnitude of the forces for each object.
-                    let d1: Vec3 = (e2.1.translation - e1.1.translation).normalize() * force_mag;
-                    let d2 = (e1.1.translation - e2.1.translation).normalize() * force_mag;
+                    let d1 = (t2.translation - t1.translation).normalize() * force_mag;
+                    let d2 = (t1.translation - t2.translation).normalize() * force_mag;
 
-                    // add these forces to a list of forces
-                    all_forces[i].push(d1);
-                    all_forces[i + j + 1].push(d2);
+                    forces[i] += d1;
+                    forces[i+j+1] += d2;
                 });
-        }
 
-        // create a new vector to hold this snapshot
-        steps.push(Vec::new());
-        // Calculate other forces and update kinimatics
-        for (i, (kin, tran, engine)) in entities.iter_mut().enumerate() {
-            // handle acceleration from ship engine
+            // handle force from ship engine
             if let Some(t) = engine {
-                all_forces[i].push(
-                    tran.rotation.mul_vec3(Vec3::Y)
+                forces[i] +=
+                    t1.rotation.mul_vec3(Vec3::Y)
                         * match t.throttle {
                             Throttle::Fixed(true) => t.max_thrust,
                             Throttle::Fixed(false) => 0.0,
                             Throttle::Variable(amount) => amount * t.max_thrust,
-                        },
-                );
+                        };
             }
+        }
 
-            // add up forces, then apply them
-            kin.acceleration = all_forces[i]
-                .iter()
-                .copied()
-                .reduce(|acc, x| acc + x)
-                .expect("0 forces")
-                / kin.mass;
+        // update kinimatics
+        steps[step]
+            .iter_mut()
+            .enumerate()
+            .for_each(|(j, (kin, trans, _))| {
+                kin.acceleration = forces[j] / kin.mass;
+                kin.velocity = kin.velocity + kin.acceleration * dt;
+                trans.translation = trans.translation + kin.velocity * dt;
+            });
 
-            kin.velocity = kin.velocity + kin.acceleration * dt;
-            tran.translation = tran.translation + kin.velocity * dt;
-
-            steps[step].push(tran.clone());
-            all_forces[i].clear();
+        forces.clear();
+        for _ in 0..entities.len() {
+            forces.push(Vec3::ZERO);
         }
     }
 
@@ -234,7 +227,11 @@ pub fn course_projection_system(
         }
     }
 
-    let steps: Vec<Transform> = steps.into_iter().flatten().collect();
+    let steps: Vec<Transform> = steps
+        .into_iter()
+        .flatten()
+        .map(|k_bod| k_bod.1)
+        .collect();
 
     for (i, (_, mut transform)) in dots.iter_mut().enumerate() {
         *transform = steps[i];
